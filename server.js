5cee49f1f1f87557264ca55a6b16db6759ceceea
Rhythm Saha <rs.2001.saha@gmail.com>
2022-07-28 10:57:29 +0530

    connected to mongodb

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 require("dotenv/config");
+require("./config/db");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 
+// Middlewares
 app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Running!");
+});
 
 // Listening to port
 const port = process.env.PORT || 8000;
