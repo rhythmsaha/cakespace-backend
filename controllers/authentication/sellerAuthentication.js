535e890aff3a4109e42969b7228e4d4353e3e9bd
Rhythm Saha <rs.2001.saha@gmail.com>
2022-08-31 08:09:38 +0530

    seller authentication

--- controllers/authentication/sellerAuthentication.js
+++ controllers/authentication/sellerAuthentication.js
@@ -2,15 +2,22 @@ const jwt = require("jsonwebtoken");
 const Seller = require("../../models/sellerModel");
 const { isEmail } = require("validator");
 const asyncHandler = require("express-async-handler");
+const { forgetPassword } = require("../../utils/forgetPassword");
 
 exports.registerSeller = asyncHandler(async (req, res) => {
-    const { fullName, email, password } = req.body;
+    const { fullName, email, password, confirmPasword } = req.body;
 
-    if (!fullName || !email || !password)
+    if (!fullName || !email || !password || !confirmPasword)
         return res.status(400).json({ type: "ALL_FIELDS", message: "Please fill the required fields!" });
 
     if (!email || !isEmail(email))
         return res.status(400).json({ type: "EMAIL", message: "Please provide valid email address!" });
+
+    if (!password || !confirmPasword)
+        return res.status(400).json({ type: "PASSWORD", message: "Password Can't be empty!" });
+
+    if (password !== confirmPasword)
+        return res.status(400).json({ type: "PASSWORD", message: "Passwords don't match!" });
 
     const seller = await Seller.findOne({ email: email });
 
@@ -46,7 +53,9 @@ exports.loginSeller = asyncHandler(async (req, res) => {
 
     if (!checkPassword) return res.status(403).json({ type: "PASSWORD", message: "Incorrect Password!" });
 
-    const JWT_TOKEN = jwt.sign({ _id: seller._id }, process.env.ADMIN_SECRET, { expiresIn: "1d" });
+    const JWT_TOKEN = jwt.sign({ role: "ADMIN", type: "AUTH_TOKEN", _id: seller._id }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+    });
 
     return res.status(200).json({
         JWT_TOKEN,
@@ -65,7 +74,12 @@ exports.getMe = asyncHandler(async (req, res) => {
 
     if (!seller) return res.status(404).json({ message: "Account Not Found!" });
 
+    const JWT_TOKEN = jwt.sign({ role: "ADMIN", type: "AUTH_TOKEN", _id: seller._id }, process.env.JWT_SECRET, {
+        expiresIn: "1d",
+    });
+
     return res.status(200).json({
+        JWT_TOKEN,
         user: {
             fullName: seller.fullName,
             email: seller.email,
@@ -74,6 +88,27 @@ exports.getMe = asyncHandler(async (req, res) => {
     });
 });
 
-exports.forgetSellerPassword = asyncHandler(async (req, res) => {});
+exports.forgetSellerPassword = asyncHandler(async (req, res) => {
+    const { email } = req.body;
+    const seller = await Seller.findOne({ email });
+    if (!seller) return res.status(404).json({ message: "User not found!" });
 
-exports.resetSellerPassword = asyncHandler(async (req, res) => {});
+    const passwordResetToken = jwt.sign(
+        { role: "ADMIN", type: "RESET_PASSWORD", _id: seller._id },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "1h",
+        }
+    );
+
+    // Send verification link to user email
+
+    return res.status(200).json({
+        message: "We have sent a link to your email to reset your password",
+    });
+});
+
+exports.resetSellerPassword = asyncHandler(async (req, res) => {
+    const { password, confirmPasword, token } = req.body;
+    if (password !== confirmPasword) return res.status(400).json({ message: "Passwords didn't match!" });
+});
