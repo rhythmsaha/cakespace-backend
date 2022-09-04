449d3fcff6ee2a3c3c3a578be63a20bd32e1d8aa
Rhythm Saha <rs.2001.saha@gmail.com>
2022-09-05 01:54:31 +0530

    redis

--- server.js
+++ server.js
@@ -4,11 +4,23 @@ const dotenv = require("dotenv");
 const app = express();
 const connectDB = require("./config/db.config");
 const cors = require("cors");
+const Redis = require("redis");
 
 const { errorHandler, notFound } = require("./middlewares/errorMiddleware");
 const authenticationRoute = require("./routes/authentication.routes");
 const categoriesRoute = require("./routes/categories.routes");
 const flavoursRoute = require("./routes/flavours.routes");
+
+const redisClient = Redis.createClient({
+    url: "redis://redis-12781.c305.ap-south-1-1.ec2.cloud.redislabs.com:12781",
+    password: "IZOZdKcFSXLwHMqRjSY0m4QSVJmmUXV6",
+});
+
+redisClient.connect();
+
+redisClient.on("connect", function () {
+    console.log("Connected!");
+});
 
 dotenv.config();
 connectDB();
@@ -21,6 +33,17 @@ app.use(express.json());
 app.use("/auth", authenticationRoute);
 app.use("/categories", categoriesRoute);
 app.use("/flavours", flavoursRoute);
+
+app.get("/test", async (req, res) => {
+    const name = await redisClient.get("name");
+    res.send(name);
+});
+
+app.get("/test/:name", async (req, res) => {
+    const { name } = req.params;
+    redisClient.set("name", name);
+    res.send("done");
+});
 
 app.use(notFound);
 app.use(errorHandler);
