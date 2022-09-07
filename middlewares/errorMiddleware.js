b00d5d4feb056821fed3c99e89471e1e62cd5b17
Rhythm Saha <rs.2001.saha@gmail.com>
2022-09-08 03:14:36 +0530

    error middleware

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,7 +1,14 @@
+const AppError = require("../utils/AppError");
+
 const notFound = (req, res, next) => {
     const error = new Error(`Not Found - ${req.originalUrl}`);
     res.status(404);
     next(error);
+};
+
+const handleCastError = (error) => {
+    const message = `Invalid ${error.path}: ${error.value}`;
+    return new AppError(message, 400);
 };
 
 const handleValidationError = (err, res) => {
@@ -12,12 +19,15 @@ const handleValidationError = (err, res) => {
 
 const errorHandler = (err, req, res, next) => {
     const statusCode = err.statusCode || 500;
+    const status = err.status || "error";
 
+    if (err.name === "CastError") error = handleCastError(error);
     if (err.name === "ValidationError") return (err = handleValidationError(err, res));
 
     res.status(statusCode);
     res.json({
         type: err?.type,
+        status: status,
         message: err.message,
         stack: process.env.NODE_ENV === "production" ? null : err.stack,
     });
