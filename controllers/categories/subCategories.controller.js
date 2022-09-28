68c26d8be232e19b8be45156a422f5a2ad9974be
Rhythm Saha <rs.2001.saha@gmail.com>
2022-09-28 12:38:50 +0530

    get subcategories function

--- controllers/categories/subCategories.controller.js
+++ controllers/categories/subCategories.controller.js
@@ -3,65 +3,74 @@ const { Category, SubCategory } = require("../../models/categories.model");
 const AppError = require("../../utils/AppError");
 
 exports.addSubCategory = asyncHandler(async (req, res) => {
-    const { role, type } = req?.user;
-    if (type !== "AUTH_TOKEN" && role !== "ADMIN") throw new AppError("Access Denied!", 403, "authorization");
+  const { role, type } = req?.user;
+  if (type !== "AUTH_TOKEN" && role !== "ADMIN") throw new AppError("Access Denied!", 403, "authorization");
 
-    const { name, enabled, categoryId } = req.body;
+  const { name, enabled, categoryId } = req.body;
 
-    const subCategory = new SubCategory({ name: name, enabled: enabled, categoryId: categoryId });
-    const saveSubCategory = await subCategory.save();
+  const subCategory = new SubCategory({ name: name, enabled: enabled, categoryId: categoryId });
+  const saveSubCategory = await subCategory.save();
 
-    if (!saveSubCategory) throw new AppError("Couldn't save new Sub category!", 500);
+  if (!saveSubCategory) throw new AppError("Couldn't save new Sub category!", 500);
 
-    const updateCategory = await Category.updateOne(
-        { _id: categoryId },
-        { $push: { subCategories: saveSubCategory._id } }
-    );
+  const updateCategory = await Category.updateOne(
+    { _id: categoryId },
+    { $push: { subCategories: saveSubCategory._id } }
+  );
 
-    if (!updateCategory) throw new AppError("Something went wrong!", 500);
+  if (!updateCategory) throw new AppError("Something went wrong!", 500);
 
-    return res.json({
-        message: "Subcategory Created!",
-        subCategory: saveSubCategory,
-    });
+  return res.json({
+    message: "Subcategory Created!",
+    subCategory: saveSubCategory,
+  });
 });
 
 exports.updateSubCategory = asyncHandler(async (req, res) => {
-    const { role, type } = req?.user;
-    if (type !== "AUTH_TOKEN" && role !== "ADMIN") throw new AppError("Access Denied!", 403, "authorization");
+  const { role, type } = req?.user;
+  if (type !== "AUTH_TOKEN" && role !== "ADMIN") throw new AppError("Access Denied!", 403, "authorization");
 
-    const { id } = req.params;
-    const { name, enabled } = req.body;
+  const { id } = req.params;
+  const { name, enabled } = req.body;
 
-    const subCategory = await SubCategory.findById(id);
-    if (!subCategory) throw new AppError("SubCategory not found!", 404);
+  const subCategory = await SubCategory.findById(id);
+  if (!subCategory) throw new AppError("SubCategory not found!", 404);
 
-    if (name) subCategory.name = name;
-    subCategory.enabled = enabled || false;
+  if (name) subCategory.name = name;
+  subCategory.enabled = enabled || false;
 
-    const updateSubCategory = await subCategory.save();
+  const updateSubCategory = await subCategory.save();
 
-    if (!SubCategory) throw new AppError("Couldn't update subcategory!", 500);
+  if (!SubCategory) throw new AppError("Couldn't update subcategory!", 500);
 
-    res.json({
-        message: "Saved Subcategory!",
-        subCategory: updateSubCategory,
-    });
+  res.json({
+    message: "Saved Subcategory!",
+    subCategory: updateSubCategory,
+  });
 });
 
 exports.deleteSubCategory = asyncHandler(async (req, res) => {
-    const { role, type } = req?.user;
-    if (type !== "AUTH_TOKEN" && role !== "ADMIN") throw new AppError("Access Denied!", 403, "authorization");
+  const { role, type } = req?.user;
+  if (type !== "AUTH_TOKEN" && role !== "ADMIN") throw new AppError("Access Denied!", 403, "authorization");
 
-    const { id } = req.params;
+  const { id } = req.params;
 
-    if (!id) throw new AppError("id is required!", 400);
+  if (!id) throw new AppError("id is required!", 400);
 
-    const deleteItem = await SubCategory.findByIdAndDelete(id);
-    if (!deleteItem) throw new AppError("Failed to delete!", 500);
-    await Category.findOneAndUpdate({ _id: deleteItem.categoryId }, { $pull: { subCategories: id } });
+  const deleteItem = await SubCategory.findByIdAndDelete(id);
+  if (!deleteItem) throw new AppError("Failed to delete!", 500);
+  await Category.findOneAndUpdate({ _id: deleteItem.categoryId }, { $pull: { subCategories: id } });
 
-    // console.log(id);
+  // console.log(id);
 
-    res.json({ message: "Successfully deleted!", subCategory: deleteItem });
+  res.json({ message: "Successfully deleted!", subCategory: deleteItem });
+});
+
+exports.getSubCategories = asyncHandler(async (req, res) => {
+  const categorySlug = req.params.slug;
+  if (!categorySlug) throw new AppError("Slug is Required", 400);
+
+  const category = await Category.findOne({ slug: categorySlug }).populate("subCategories").select("subCategories");
+  if (!category) throw new AppError("No category found!", 404);
+  return res.status(200).json(category.subCategories);
 });
--- routes/subcategories.routes.js
+++ routes/subcategories.routes.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const { authorize } = require("../middlewares/authorize");
 
 const {
-    addSubCategory,
-    updateSubCategory,
-    deleteSubCategory,
+  addSubCategory,
+  updateSubCategory,
+  deleteSubCategory,
+  getSubCategories,
 } = require("../controllers/categories/subCategories.controller");
 
+router.get("/:slug", getSubCategories);
 router.post("/", authorize, addSubCategory);
 router.patch("/:id", authorize, updateSubCategory);
 router.delete("/:id", authorize, deleteSubCategory);
