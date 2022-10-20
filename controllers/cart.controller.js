6fef358239369c610c82124184bbbd67409c0165
Rhythm Saha <rs.2001.saha@gmail.com>
2022-10-20 09:08:01 +0530

    cart api

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -71,12 +71,9 @@ exports.addItemToCart = expressAsyncHandler(async (req, res) => {
   res.send({ message: "Added to Cart!", cart: saveCart });
 });
 
-exports.deleteItem = expressAsyncHandler(async (req, res) => {});
-exports.deleteAll = expressAsyncHandler(async (req, res) => {});
-
 exports.removeItemFromCart = expressAsyncHandler(async (req, res) => {
   const { productId } = req.body;
-  const { role, _id } = req.user;
+  const { _id } = req.user;
 
   if (!productId) throw new AppError("ProductId is required!", 400);
 
@@ -85,23 +82,46 @@ exports.removeItemFromCart = expressAsyncHandler(async (req, res) => {
 
   if (!existingItem) throw new AppError("Product doesn't exist in cart!", 400);
 
-  // if (existingItem.quantity > 1) {
-  //   const product = await Product.findById(productId);
-  //   const item = { ...existingItem };
+  if (existingItem.quantity > 1) {
+    const product = await Product.findById(productId);
+    const item = { ...existingItem };
 
-  //   item.quantity--;
-  //   item.price = item.quantity * product.price;
+    item.quantity--;
+    item.price = item.quantity * product.price;
 
-  //   const cartItems = cart.items.map((_item) => {
-  //     if (_item.product._id === productId) return item;
-  //     else return _item;
-  //   });
+    const cartItems = cart.items.map((_item) => {
+      if (_item.product._id === productId) return item;
+      else return _item;
+    });
 
-  //   cart.items = cartItems;
-  // } else {
-  //   const cartItems = cart.items.filter((_item) => _item.product._id !== productId);
-  //   cart.items = cartItems;
-  // }
+    cart.items = cartItems;
+  } else {
+    const cartItems = cart.items.filter((_item) => _item.product._id !== productId);
+    cart.items = cartItems;
+  }
+
+  cart.totalQuantity = cart.items.reduce((value, _item) => value + _item.quantity, 0);
+  cart.totalAmount = cart.items.reduce((value, _item) => value + _item.totalPrice, 0);
+
+  const saveCart = await cart.save().then((__cart) => __cart.populate("items.product"));
+
+  res.status(200).json({
+    success: true,
+    message: "Successfully removed item!",
+    cart: saveCart,
+  });
+});
+
+exports.deleteItem = expressAsyncHandler(async (req, res) => {
+  const { productId } = req.body;
+  const { _id } = req.user;
+
+  if (!productId) throw new AppError("ProductId is required!", 400);
+
+  let cart = await Cart.findOne({ user: _id });
+  const existingItem = cart.items.find((item) => item.product.equals(productId));
+
+  if (!existingItem) throw new AppError("Product doesn't exist in cart!", 400);
 
   const cartItems = cart.items.filter((_item) => _item.product.toString() !== productId);
   cart.items = cartItems;
@@ -118,7 +138,7 @@ exports.removeItemFromCart = expressAsyncHandler(async (req, res) => {
 });
 
 exports.clearCart = expressAsyncHandler(async (req, res) => {
-  const { role, _id } = req.user;
+  const { _id } = req.user;
 
   let cart = await Cart.findOne({ user: _id });
 
--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getCart, addItemToCart, removeItemFromCart, clearCart } = require("../controllers/cart.controller");
+const { getCart, addItemToCart, removeItemFromCart, clearCart, deleteItem } = require("../controllers/cart.controller");
 const { authorize } = require("../middlewares/authorize");
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get("/", authorize, getCart);
 router.put("/", authorize, addItemToCart);
 router.delete("/", authorize, removeItemFromCart);
+router.delete("/remove", authorize, deleteItem);
 router.delete("/clear", authorize, clearCart);
 
 module.exports = router;
