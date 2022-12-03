3e42134e008eb9e5a0699a50950bb33b39d44aac
Rhythm Saha <rs.2001.saha@gmail.com>
2022-12-03 22:20:08 +0530

    webhook

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -2,25 +2,24 @@ require("dotenv/config");
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const expressAsyncHandler = require("express-async-handler");
 const Cart = require("../models/cart.model");
+const User = require("../models/user.model");
 const AppError = require("../utils/AppError");
-
-const calculateOrderAmount = async (userId) => {
-  const cart = await Cart.findOne({ user: userId });
-  if (cart.totalQuantity === 0) {
-    new AppError("Cart is empty", 400);
-  }
-
-  const totalPrice = +cart.totalAmount * 100;
-  return totalPrice;
-};
 
 exports.createPaymentIntent = expressAsyncHandler(async (req, res) => {
   const { _id } = req.user;
 
+  if (!_id) new AppError("Access Denied", 400);
+
+  const cart = await Cart.findOne({ user: _id });
+
+  if (cart.totalQuantity === 0) new AppError("Cart is empty", 400);
+
+  const totalPrice = +cart.totalAmount * 100;
+
   // Create a PaymentIntent with the order amount and currency
 
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: await calculateOrderAmount(_id),
+    amount: totalPrice,
     currency: "inr",
     automatic_payment_methods: {
       enabled: true,
@@ -34,4 +33,56 @@ exports.createPaymentIntent = expressAsyncHandler(async (req, res) => {
     orderId: paymentIntent.created,
     amount: paymentIntent.amount,
   });
+});
+
+exports.webhook = expressAsyncHandler(async (request, response) => {
+  let event = request.body;
+  console.log(`webhook init`);
+  console.log(event.type);
+
+  // // Only verify the event if you have an endpoint secret defined.
+  // // Otherwise use the basic event deserialized with JSON.parse
+  // if (endpointSecret) {
+  //   // Get the signature sent by Stripe
+  //   console.log("webhook signature");
+  //   const signature = request.headers["stripe-signature"];
+  //   try {
+  //     event = stripe.webhooks.constructEvent(request.body, signature, endpointSecret);
+  //   } catch (err) {
+  //     console.log(`⚠️  Webhook signature verification failed.`, err.message);
+  //     return response.sendStatus(400);
+  //   }
+  // }
+
+  // // Handle the event
+
+  switch (event.type) {
+    case "payment_intent.succeeded":
+      const paymentIntent = event.data.object;
+      console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+
+      // Then define and call a method to handle the successful payment intent.
+      // handlePaymentIntentSucceeded(paymentIntent);
+
+      const user = await User.findOne({ email: paymentIntent.receipt_email });
+      let cart = await Cart.findOne({ user: user._id });
+
+      cart.items = [];
+      cart.totalQuantity = 0;
+      cart.totalAmount = 0;
+
+      await cart.save();
+      break;
+    case "payment_method.attached":
+      const paymentMethod = event.data.object;
+      // Then define and call a method to handle the successful attachment of a PaymentMethod.
+      // handlePaymentMethodAttached(paymentMethod);
+      break;
+    default:
+      // Unexpected event type
+      console.log(`Unhandled event type ${event.type}.`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
 });
--- routes/checkout.routes.js
+++ routes/checkout.routes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const { authorize } = require("../middlewares/authorize");
-const { createPaymentIntent } = require("../controllers/payment.controller");
+const { createPaymentIntent, webhook } = require("../controllers/payment.controller");
 
 router.post("/create-payment-intent", authorize, createPaymentIntent);
+router.post("/webhook", webhook);
 
 module.exports = router;
